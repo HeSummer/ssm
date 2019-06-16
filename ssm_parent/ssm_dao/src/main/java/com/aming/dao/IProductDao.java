b5e3be540f47eca9ddccddfc0228e04310fd0afb
HeSummer <18821755390@163.com>
2019-06-16 14:20:49 +0800

    删除问题代码，重写

--- ssm_parent/ssm_dao/src/main/java/com/aming/dao/IProductDao.java
+++
@@ -1,9 +0,0 @@
-package com.aming.dao;
-
-import com.aming.domain.Product;
-
-import java.util.List;
-
-public interface IProductDao {
-     List<Product> findAll()throws Exception;;
-}
--- ssm_parent/ssm_service/src/main/java/com/aming/service/IProductService.java
+++
@@ -1,9 +0,0 @@
-package com.aming.service;
-
-import com.aming.domain.Product;
-
-import java.util.List;
-
-public interface IProductService {
-    List<Product> findAll() throws Exception;
-}
